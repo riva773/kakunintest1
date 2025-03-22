7a9129db97eb403ac2a829efc6feea015d691257
riva773 <teslaicaer@gmail.com>
2025-03-22 14:38:07 +0900

    thanksページの作成(デザイン以外)

--- src/resources/views/thanks.blade.php
+++ src/resources/views/thanks.blade.php
@@ -3,10 +3,14 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="{{ asset('/css/thanks.css') }}">
     <title>Thanks</title>
 </head>
 <body>
-    <h2>お問い合わせありがとうございました</h2>
-    <a href="/">HOME</a>
+    <div class="thanks__background">Thank you</div>
+    <div class="thanks__container">
+        <p class="thanks__message">お問い合わせありがとうございました</p>
+        <a href="/" class="thanks__button">HOME</a>
+    </div>
 </body>
 </html>
