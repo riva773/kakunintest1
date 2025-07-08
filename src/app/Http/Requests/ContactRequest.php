b92034068063121d0ff73d3469f4fdbf83a601b5
riva773 <teslaicaer@gmail.com>
2025-07-09 00:26:47 +0900

    修正

--- src/app/Http/Requests/ContactRequest.php
+++ src/app/Http/Requests/ContactRequest.php
@@ -19,9 +19,6 @@ class ContactRequest extends FormRequest
             'last_name' => 'required',
             'gender' => 'required',
             'email' => ['required', 'email:strict,dns'],
-            'phone1' => ['required', 'regex:/^[0-9]+$/', 'max:5'],
-            'phone2'=> ['required', 'regex:/^[0-9]+$/', 'max:5'],
-            'phone3' => ['required', 'regex:/^[0-9]+$/', 'max:5'],
             'address' => 'required',
             'category_id' => 'required',
             'detail' => ['required', 'max:120']
@@ -35,16 +32,32 @@ class ContactRequest extends FormRequest
             'gender.required' => '性別を選択してください',
             'email.required' => 'メールアドレスを入力してください',
             'email.email' => 'メールアドレスはメール形式で入力してください',
-            'phone1.required' => '電話番号を入力してください',
-            'phone2.required' => '電話番号を入力してください',
-            'phone3.required' => '電話番号を入力してください',
-            'phone1.max' => '電話番号は5桁までの数字で入力してください',
-            'phone2.max' => '電話番号は5桁までの数字で入力してください',
-            'phone3.max' => '電話番号は5桁までの数字で入力してください',
             'address.required' => '住所を入力してください',
             'category_id.required' => 'お問い合わせの種類を選択してください',
             'detail.required' => 'お問い合わせ内容を入力してください',
             'detail.max' => 'お問合せ内容は120文字以内で入力してください',
         ];
     }
+
+    public function withValidator($validator)
+{
+    $validator->after(function ($validator) {
+        $phone1 = $this->input('phone1');
+        $phone2 = $this->input('phone2');
+        $phone3 = $this->input('phone3');
+
+        if (empty($phone1) || empty($phone2) || empty($phone3)) {
+            $validator->errors()->add('phone', '電話番号を入力してください');
+        }
+
+        $isInvalid = function ($val) {
+            return !preg_match('/^\d{1,5}$/', $val ?? '');
+        };
+
+        if ($isInvalid($phone1) || $isInvalid($phone2) || $isInvalid($phone3)) {
+            $validator->errors()->add('phone_format', '電話番号は5桁までの数字で入力してください');
+        }
+    });
+}
+
 }
